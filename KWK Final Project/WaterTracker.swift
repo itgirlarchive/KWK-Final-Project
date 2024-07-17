93db5374c8a52b983ad0b98a33d3e661e13349a1
Anusha Mahajan <anushamahajan505@gmail.com>
2024-07-17 13:58:21 -0700

    Added background color and tried to make button that corresponds to adding to the variable water consumption, which would in theory control how high the water rectangle goes up

--- KWK Final Project/WaterTracker.swift
+++ KWK Final Project/WaterTracker.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct WaterTracker: View {
+    var waterCons = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        
+        //func fillBody() {
+            
+          //  .position(x: 10, y: 125)
+
+        //}
+        ZStack {
+            Color("BackColor").ignoresSafeArea()
+            //background color
+            //water that fills the body (blue rectangles)
+            //photo of the human body
+            VStack {
+                /*   var motivationQuote = ["WaveHeat", "RecipesIcon"]
+                 var randNum = Int.random(in: 0...1)
+                 Image(motivationQuote[randNum])
+                 .resizable(resizingMode: .stretch)
+                 .aspectRatio(contentMode: .fit)
+                 .padding(.bottom)
+                 */
+               Button("Fill") {
+                  //  fillBody()
+                }
+                    //var waterCons +=
+            }
+
+        }
+        }
 }
 
 #Preview {
