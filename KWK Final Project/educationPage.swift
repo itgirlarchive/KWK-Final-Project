00f03b3df706fa6bfdf782ea05887bb8c6d22f12
Anika <prakashanika5@gmail.com>
2024-07-18 14:43:20 -0700

    add description about water conservation

--- KWK Final Project/educationPage.swift
+++ KWK Final Project/educationPage.swift
@@ -15,49 +15,76 @@ struct informationPage: View {
     
     var body: some View {
         
-        ZStack {
-            Color("BackColor").ignoresSafeArea()
-            
-            VStack {
-                Spacer()
-                    .frame(height: 20)
-                Text("Why is Water     Conservation Important?")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.center)
-                    .padding()
-                    .background(Rectangle() .foregroundColor(.white))
-                    .cornerRadius(15)
-                    .shadow(radius: 15)
-                    .padding()
-                Spacer()
+        NavigationStack {
+            ZStack {
+                Color("BackColor").ignoresSafeArea()
                 
-                Image(images[currentIndex])
-                    .resizable(resizingMode: .stretch)
-                    .aspectRatio(contentMode: .fit)
-                
-                HStack {
-                    // the loop below creates circles for each image
-                    ForEach (0..<4) { index in
-                        Circle()
-                            .fill(self.currentIndex == index ? Color.gray : Color.black)
-                            .frame(width: 10, height: 10)
+                VStack {
+                    Spacer()
+                        .frame(height: 20)
+                    Text("Why is Water     Conservation Important?")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .background(Rectangle() .foregroundColor(.white))
+                        .cornerRadius(15)
+                        .shadow(radius: 15)
+                        .padding()
+                    Spacer()
+                        .frame(height: 30)
+                    
+                    Image(images[currentIndex])
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fill)
+                        .frame(height: 270)
+                    
+                    HStack {
+                        // the loop below creates circles for each image
+                        ForEach (0..<4) { index in
+                            Circle()
+                                .fill(self.currentIndex == index ? Color.gray : Color.black)
+                                .frame(width: 10, height: 10)
+                        }
+                    }
+                    
+                    Spacer()
+                        .frame(height: 10)
+                    
+                    ScrollView {
+                        Text("It is is important to be mindful about water consumption and the way we use water. Around the world, over 2 billion people are forced to live without access to safely managed drinking water services. As the world is facing increased drought and water pollution, remember to conserve water.")
+                            .font(.title3)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal, 10)
+                    }
+                    
+                    Spacer()
+                        .frame(height: 10)
+                    
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Button ("Take Water Conservation Quiz ➡️") {}
+                            .background(Rectangle() .foregroundColor(.white))
+                            .shadow(radius: 5)
+                            .padding(.all)
+                            .foregroundColor(.black)
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                    }
+                    
+                    
+                }.onAppear {
+                    // uses Timers to get the photos to cycle
+                    Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { timer in
+                        if self.currentIndex + 1 == self.images.count{
+                            self.currentIndex = 0
+                        } else {
+                            self.currentIndex += 1
+                        }
                     }
                 }
+                .padding(20)
                 
-                Spacer()
-                
-            }.onAppear {
-                // uses Timers to get the photos to cycle
-                Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { timer in
-                    if self.currentIndex + 1 == self.images.count{
-                        self.currentIndex = 0
-                    } else {
-                        self.currentIndex += 1
-                    }
-                }
             }
-            .padding()
             
         }
         
