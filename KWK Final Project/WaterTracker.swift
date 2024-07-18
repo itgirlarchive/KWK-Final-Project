34c262451c2d3dfd83c037605bfe1f0d5ea0606d
Anusha Mahajan <anushamahajan505@gmail.com>
2024-07-17 18:45:08 -0700

    Added transparent body to assets, fixed up the motivational quote (still has work tho), worked on a text field & button (both non functioning tho), and inserted the body in (still need to put blue rectangle for water behind)

--- KWK Final Project/WaterTracker.swift
+++ KWK Final Project/WaterTracker.swift
@@ -10,28 +10,62 @@ import SwiftUI
 struct WaterTracker: View {
     var waterCons = 0
     var body: some View {
-        
-        //func fillBody() {
+     /*   let quoteList = ["Keep up the great work!", "You can do it!", "Almost there!", "You got this!", "Do not give up!", "Doing amazing!"]
+        var randNum = Int.random(in: 0...6)
+      func fillBody() {
             
-          //  .position(x: 10, y: 125)
+          .position(x: 10, y: 125)
 
-        //}
+        }
+      */
         ZStack {
             Color("BackColor").ignoresSafeArea()
             //background color
             //water that fills the body (blue rectangles)
             //photo of the human body
             VStack {
-                /*   var motivationQuote = ["WaveHeat", "RecipesIcon"]
-                 var randNum = Int.random(in: 0...1)
-                 Image(motivationQuote[randNum])
-                 .resizable(resizingMode: .stretch)
-                 .aspectRatio(contentMode: .fit)
-                 .padding(.bottom)
-                 */
-               Button("Fill") {
+                Spacer()
+                HStack {
+                    Text("Keep up the good work!")
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                        .padding(.all)
+                        .background(Rectangle() .foregroundColor(.white))
+                        .cornerRadius(15)
+                        .shadow(radius: 15)
+                        .padding(.vertical)
+                        .padding(.top)
+                        .padding(.vertical)
+
+
+                }
+                Spacer()
+                
+                TextField("Water Consumed in Ounces", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .padding(.vertical)
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .padding(.all)
+
+                
+                Button("Fill") {
                   //  fillBody()
                 }
+                .padding(.all)
+                .background(Rectangle() .foregroundColor(.white))
+                .cornerRadius(15)
+                .font(.title)
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("TransparentBody")
+                        .renderingMode(.original)
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fill)
+                        .padding(.trailing)
+                }
+                
                     //var waterCons +=
             }
 
